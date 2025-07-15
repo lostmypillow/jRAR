1e1f6c55e03ef1773e046a138668741ed9ca35a1
Johnny <jmlin0101@gmail.com>
2025-07-15 14:44:25 +0800

    Update settings.py

--- jrar/settings.py
+++ jrar/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['jrar.lostmypillow.duckdns.org', 'localhost', '0.0.0.0', 'jrar-latest.onrender.com', 'lostmypillow.github.io']
+ALLOWED_HOSTS = ['jrar.lostmypillow.duckdns.org', 'localhost', '0.0.0.0', 'jrar-latest.onrender.com', 'lostmypillow.github.io', 'jrar.pages.dev']
 
 
 # Application definition
